3e19da56d18a5a3a0fd425b283c9b64ad3b5eefe
Bryan Lee <leebryan.perso@gmail.com>
2020-12-03 11:28:57 +0100

    💬 Add building hitpoints to BuildingHeader

--- src/components/BuildingHeader.tsx
+++ src/components/BuildingHeader.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Building } from "../gameData";
+import React from 'react';
+import { Building } from '../gameData';
 
 type Props = {
   building: Building;
@@ -8,26 +8,31 @@ type Props = {
 
 export function BuildingHeader(props: Props) {
   return (
-    <div className="font-bold flex items-center">
+    <div className="flex items-center">
       <img
         className="h-12 mr-3"
         src={
           // ignore level if archer queen, warden statue or royal champion
-          props.building.id === "62" ||
-          props.building.id === "63" ||
-          props.building.id === "122"
+          props.building.id === '62' ||
+          props.building.id === '63' ||
+          props.building.id === '122'
             ? `/img/${props.building.id}.png`
             : `/img/${props.building.id}_${props.level}.png`
         }
         alt={`${props.building.name}`}
         draggable="false"
       />
-      <span className="text-blue-700 dark:text-blue-300">
-        {props.building.name}
-      </span>
-      <span className="text-gray-800 dark:text-gray-100">
-        &nbsp;(level {props.level})
-      </span>
+      <div className="flex-column">
+        <span className="font-bold text-blue-700 dark:text-blue-300">
+          {props.building.name}
+        </span>
+        <span className="font-bold text-gray-800 dark:text-gray-100">
+          &nbsp;(level {props.level})
+        </span>
+        <p className="text-gray-800 dark:text-gray-100">
+          ❤ {props.building.hp[props.level - 1]}
+        </p>
+      </div>
     </div>
   );
 }
