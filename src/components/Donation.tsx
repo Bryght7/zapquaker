e5b0b94837690b2b14f01c39856ad173e44d4e89
Bryan Lee <leebryan.perso@gmail.com>
2022-05-04 23:07:25 +0200

    🐛 Fix donate image not showing on Firefox

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -33,6 +33,7 @@ export function Donation(props: Props) {
       >
         <img
           alt="Buy me a coffee"
+          className="h-12"
           src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=Bryght7&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00"
         />
       </a>
