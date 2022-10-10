4ba027b427aac8c93bc0187f628922e286e935a4
Bryan Lee <leebryan.perso@gmail.com>
2022-10-11 01:39:03 +0200

    🎮 Update game data for TH15 update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 export function Footer(props: Props) {
   return (
     <footer className="flex flex-col items-center py-4 bg-white border-t dark:bg-gray-800 dark:border-gray-500 dark:text-blue-100">
-      <p className="mb-4">✔ Last update: June 2022 Update</p>
+      <p className="mb-4">✔ Last update: TH15 Update</p>
       <Donation></Donation>
     </footer>
   );
