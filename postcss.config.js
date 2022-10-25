575e99deb733d46e8ce93c3163fbebeef9564396
Bryan Lee <leebryan.perso@gmail.com>
2022-10-26 01:51:30 +0200

    ⚡️ Add Tailwind optimizing for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ]
-  }
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
